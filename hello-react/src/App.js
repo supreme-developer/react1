e3f531750048a6b6dbcab27027903a7b71c1a3ae
jewooSong <iamsjy0808@gmail.com>
2019-07-25 02:39:44 +0900

    [추가] 예제4 state와 useState

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import MyComponent1 from "./component/MyComponent1";
 import MyComponent2 from "./component/MyComponent2";
 import MyComponent3 from "./component/MyComponent3";
+import MyComponent4 from "./component/MyComponent4";
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <MyComponent2 name="송타" age={30} />
         <MyComponent3 name="react" color="red" isSpecial={true} />
         <MyComponent3 name="react" color="blue" isSpecial={false} />
+        <MyComponent4 />
       </div>
     );
   }
